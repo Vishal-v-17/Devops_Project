7b9d321cac50767ea4b421c33eae9db5e9d00464
Vishal V <x24331627@student.ncirl.ie>
2025-12-05 23:41:08 +0000

    secret key

--- Library_project/settings.py
+++ Library_project/settings.py
@@ -16,7 +16,6 @@ load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
