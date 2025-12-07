b864d79aacad7cf9c5d652e85047e54ab817ba3d
EC2 Default User <ec2-user@ip-172-31-85-192.ec2.internal>
2025-12-07 21:27:40 +0000

    resolving high sonarcube issue

--- Library_project/settings.py
+++ Library_project/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-#SECRET_KEY = 'uk3_zv-o5vq$pu=k@vytmizvemdw6=uo)kdf44a*p0c^a)bm6k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
