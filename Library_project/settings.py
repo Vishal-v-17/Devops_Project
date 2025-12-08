dfd7b93ed4b8e49e860708bcef79e6168ece72ce
EC2 Default User <ec2-user@ip-172-31-85-192.ec2.internal>
2025-12-08 16:42:01 +0000

    fixing eb

--- Library_project/settings.py
+++ Library_project/settings.py
@@ -14,9 +14,10 @@ import os
 import sys
 from dotenv import load_dotenv
 load_dotenv()
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
